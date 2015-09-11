47f645cb279a10568e5e757b1d27964d98700f96
xu-hong <xuhong713@gmail.com>
2015-09-11 12:14:58 -0400

    add get labels funciton

--- R/pk_terms.R
+++ R/pk_terms.R
@@ -14,16 +14,29 @@
 #' @examples
 #'
 
-gk_term_details <- function(iri, verbose=TRUE) {
+pk_term_detail <- function(iri, verbose=TRUE) {
   mssg(verbose, "Retrieving term IDs...")
 
-  queryseq <- list(iri=iri)
-  res <- GET("http://kb.phenoscape.org/api/term", query=queryseq)
+  queryseq <- list(iri = iri)
+  res <- GET("http://kb.phenoscape.org/api/term", query = queryseq)
   stop_for_status(res)
-  out <- content(res, as="text")
+  out <- content(res, as = "text")
 
   lst <- jsonlite::fromJSON(out, simplifyVector = TRUE, flatten = TRUE)
   # result <- dplyr::tbl_df(data.frame(lst))
+
+  return(lst)
+}
+
+pk_get_labels <- function(...) {
+  iris <- paste(..., sep=",")
+
+  queryseq <- list(iris = iris)
+  res <- GET("http://kb.phenoscape.org/api/term/labels", query = queryseq)
+  stop_for_status(res)
+  out <- content(res, as = "text")
+
+  lst <- jsonlite::fromJSON(out, simplifyVector = TRUE, flatten = TRUE)
 
   return(lst)
 }
