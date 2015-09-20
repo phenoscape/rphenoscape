8abfdfac85f66953e2ff4ff895c590e29fb48944
xu-hong <xuhong713@gmail.com>
2015-09-19 23:45:16 -0400

    improveon term search function

--- R/pk_terms.R
+++ R/pk_terms.R
@@ -12,7 +12,7 @@
 #'
 #' @export
 #' @rdname pk_terms
-pk_taxa_detail <- function(term, verbose=TRUE) {
+pk_taxon_detail <- function(term, verbose=TRUE) {
   pk_details(term, as = "vto", verbose)
 }
 #' @export
@@ -27,16 +27,12 @@ pk_phenotype_detail <- function(term, verbose=TRUE) {
 }
 
 pk_details <- function(term, as, verbose=TRUE) {
-  iri_df <- pk_get_iri(term, as, verbose)
-  mssg(verbose, "Retrieving details...")
-  if (length(iri_df) == 0) {
-    mssg(TRUE, paste("Could not find", term, "in the database."))
-    return(invisible(FALSE))
-  }
-  # naively take the first result
-  iri <- iri_df[1, "@id"]
-  queryseq <- list(iri = iri)
+  iri <- pk_get_iri(term, as, verbose)
+  if (iri == FALSE) return(invisible(FALSE))
 
+  mssg(verbose, "Retrieving details")
+
+  queryseq <- list(iri = iri)
   lst <- pk_GET("http://kb.phenoscape.org/api/term", queryseq, verbose)
   dplyr::as_data_frame(lst)
 }
@@ -54,17 +50,23 @@ pk_get_iri <- function(text, as, verbose=TRUE, limit=10) {
   mssg(verbose, paste("Querying the IRI for", text, sep = " "))
   as_type <- match.arg(as, c("vto", "uberon", "pato"))
   onto_id <- switch(as_type,
-                    vto = taxa_id(),
+                    vto = taxon_id(),
                     uberon = anatomical_id(),
                     pato = phenotype_id())
 
   queryseq <- list(text = text, definedBy = onto_id, limit = limit )
   lst <- pk_GET('http://kb.phenoscape.org/api/term/search_classes', query = queryseq)
+  iri_df <- lst$results
 
-  return(lst$results)
+  if (length(iri_df) == 0) {
+    mssg(TRUE, paste("Could not find", text, "in the database."))
+    return(invisible(FALSE))
+  }
+  # naively take the first result
+  iri_df[1, "@id"]
 }
 
-taxa_id <- function() 'http://purl.obolibrary.org/obo/vto.owl'
+taxon_id <- function() 'http://purl.obolibrary.org/obo/vto.owl'
 anatomical_id <- function() 'http://purl.obolibrary.org/obo/uberon.owl'
 phenotype_id <- function() 'http://purl.obolibrary.org/obo/pato.owl'
 
