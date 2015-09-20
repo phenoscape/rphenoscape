b1b7913bc0b7c8816e0c9f9f494ff22103d9a630
xu-hong <xuhong713@gmail.com>
2015-09-19 23:46:10 -0400

    add unittests

--- tests/testthat/test-pk.R
+++ tests/testthat/test-pk.R
@@ -1,24 +1,29 @@
 context("rphenoscape test")
 
 test_that("Test term search", {
-  a <- pk_taxa_detail("coral")
+  a <- pk_taxon_detail("coral")
   b <- pk_phenotype_detail("shape")
   c <- pk_anatomical_detail("fin")
+
+  aa <- pk_taxon_detail("coral tt")
+  bb <- pk_phenotype_detail("shape tt")
+  cc <- pk_anatomical_detail("fin tt")
 
   expect_output(str(a), "Classes ‘tbl_df’, ‘tbl’ and 'data.frame'")
   expect_output(str(b), "Classes ‘tbl_df’, ‘tbl’ and 'data.frame'")
   expect_output(str(c), "Classes ‘tbl_df’, ‘tbl’ and 'data.frame'")
+
+  expect_equal(aa, FALSE)
+  expect_equal(bb, FALSE)
+  expect_equal(cc, FALSE)
 })
 
 test_that("Test retriving IRI", {
   i <- pk_get_iri("Coralliozetus", "vto")
   ii <- pk_get_iri("Coralliozetus TT", "vto")
   iii <- pk_get_iri("Coralliozetus", "pato")
-  expect_output(str(i), 'data.frame')
-  expect_equal(i[1, '@id'], "http://purl.obolibrary.org/obo/VTO_0042955")
-  expect_output(str(ii), 'list')
-  expect_error(ii[1, '@id'])
-  expect_output(str(iii), 'list')
-  expect_error(iii[1, '@id'])
 
+  expect_equal(i, "http://purl.obolibrary.org/obo/VTO_0042955")
+  expect_equal(ii, FALSE)
+  expect_equal(iii, FALSE)
 })
