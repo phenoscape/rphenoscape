fb7adaf32e1a7d11027bf5eac23b991ea8607bb5
xu-hong <xuhong713@gmail.com>
2015-11-01 17:25:59 -0500

    add skeleton for if_descendant

---
+++ R/pk_if_descendant.R
@@ -0,0 +1,17 @@
+#' pk_if_descendant
+#' @param taxon
+#' @param ids, a list or vector of taxon IRIs
+#' @return a list of matching ids
+#'
+#' @name pk_if
+#'
+#' @export
+#' @rdname pk_if
+pk_if_descendant <- function(taxon, ids) {
+
+}
+#' @export
+#' @rdname pk_if
+pk_if_ancestor <- function(taxon, ids) {
+
+}
