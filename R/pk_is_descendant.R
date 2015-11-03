cd0261724c516a1b06f3fbf1003f6a3e738070f8
xu-hong <xuhong713@gmail.com>
2015-11-02 21:28:03 -0500

    rename the funciton

---
+++ R/pk_is_descendant.R
@@ -0,0 +1,17 @@
+#' pk_is_descendant
+#' @param taxon
+#' @param ids, a list or vector of taxon IRIs
+#' @return a list of matching ids
+#'
+#' @name pk_desc
+#'
+#' @export
+#' @rdname pk_desc
+pk_is_descendant <- function(taxon, candidates) {
+  #TODO: https://github.com/phenoscape/phenoscape-kb-services/issues/14
+}
+#' @export
+#' @rdname pk_desc
+pk_is_ancestor <- function(taxon, ids) {
+
+}
