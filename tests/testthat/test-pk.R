265f6d17c4e8e1f3f09d5b38f91cd490c75dd843
Hilmar Lapp <hlapp@drycafe.net>
2018-10-23 22:06:16 -0400

    Adds extensive tests for rewritten get_study_list()

--- tests/testthat/test-pk.R
+++ tests/testthat/test-pk.R
@@ -104,14 +104,76 @@ test_that("Test OnToTrace", {
 #
 test_that("Test getting study information", {
     skip_on_cran()
-    slist <- pk_get_study_list(taxon = "Ameiurus", entity = "pelvic splint")
-    s1 <- pk_get_study_xml('https://scholar.google.com/scholar?q=The+Phylogeny+of+Ictalurid+Catfishes%3A+A+Synthesis+of+Recent+Work&btnG=&hl=en&as_sdt=0%2C42')
-    ss1 <- pk_get_study(s1)
-    sss1 <- pk_get_study_meta(s1)
+    # backwards compatible mode, defaults to including part_of
+    slist1 <- pk_get_study_list(taxon = "Siluridae", entity = "fin")
+    expect_is(slist1, "data.frame")
+    expect_gt(nrow(slist1), 0)
 
-    expect_is(slist, 'data.frame')
+    # only subsumption, no parts or other relationships
+    slist2 <- pk_get_study_list(taxon = "Siluridae", entity = "fin", includeRels = FALSE)
+    expect_is(slist2, "data.frame")
+    expect_gt(nrow(slist2), 0)
+    expect_gt(nrow(slist1), nrow(slist2))
+
+    # all supported relationships
+    slist3 <- pk_get_study_list(taxon = "Siluridae", entity = "fin", includeRels = TRUE)
+    expect_is(slist3, "data.frame")
+    expect_gt(nrow(slist3), 0)
+    expect_gt(nrow(slist3), nrow(slist2))
+    expect_gte(nrow(slist3), nrow(slist1))
+
+    # subsumption and part_of relationships
+    slist4 <- pk_get_study_list(taxon = "Siluridae", entity = "fin", includeRels = c("part of"))
+    expect_is(slist4, "data.frame")
+    expect_gt(nrow(slist4), 0)
+    expect_gt(nrow(slist4), nrow(slist2))
+    expect_equal(nrow(slist4), nrow(slist1))
+
+    # using prefixes for relationship names works
+    slist5 <- pk_get_study_list(taxon = "Siluridae", entity = "fin",
+                                includeRels = c("part", "historical", "serial"))
+    expect_is(slist5, "data.frame")
+    expect_gt(nrow(slist5), 0)
+    expect_gt(nrow(slist5), nrow(slist2))
+    expect_equal(nrow(slist5), nrow(slist3))
+
+    # filtering by quality works as well
+    slist6 <- pk_get_study_list(taxon = "Siluridae", entity = "fin", quality = "size")
+    expect_is(slist6, "data.frame")
+    expect_gt(nrow(slist6), 0)
+    expect_lt(nrow(slist6), nrow(slist4))
+
+    # can also obtain all studies for taxon
+    slist7.1 <- pk_get_study_list(taxon = "Siluriformes")
+    slist7.2 <- pk_get_study_list(taxon = "Siluriformes", includeRels = FALSE)
+    expect_is(slist7.1, "data.frame")
+    expect_gt(nrow(slist7.1), 0)
+    expect_gt(nrow(slist7.1), 2 * nrow(slist3))
+    expect_equal(nrow(slist7.1), nrow(slist7.2))
+
+    # can also obtain all studies for entity
+    slist8.1 <- pk_get_study_list(entity = "pelvic fin")
+    slist8.2 <- pk_get_study_list(entity = "pelvic fin", includeRels = FALSE)
+    slist8.3 <- pk_get_study_list(entity = "pelvic fin", includeRels = c("serial","historical"))
+    expect_is(slist8.1, "data.frame")
+    expect_gt(nrow(slist8.1), nrow(slist3))
+    expect_gt(nrow(slist8.1), nrow(slist7.1))
+    expect_gt(nrow(slist8.1), nrow(slist8.2))
+    expect_gt(nrow(slist8.3), nrow(slist8.2))
+
+    # can also obtain all studies by leaving off all filters
+    slist9 <- pk_get_study_list()
+    expect_is(slist9, "data.frame")
+    expect_gt(nrow(slist9), 0)
+    expect_gt(nrow(slist9), 20 * nrow(slist3))
+
+    s1 <- pk_get_study_xml(slist1[1,"id"])
     expect_is(s1[[1]], 'nexml')
+
+    ss1 <- pk_get_study(s1)
     expect_is(ss1[[1]], 'data.frame')
+
+    sss1 <- pk_get_study_meta(s1)
     expect_is(sss1[[1]], 'list')
     expect_is(sss1[[1]]$id_taxa, 'data.frame')
 
