a051002044fa18e504343a80cd3272129afe7955
Hilmar Lapp <hlapp@drycafe.net>
2019-09-18 12:50:10 -0400

    Revises proposed function to integrate fully into package
    Also, assigns a custom class to the returned list, and a corresponding
    S3 method for pretty-printing.

--- R/get_KBinfo.R
+++ R/get_KBinfo.R
@@ -1,17 +1,43 @@
+#' Summary and metadata about the Phenoscape KB
+#'
 #' Returns the date of the current release of the Phenoscape Knowledgebase (KB) and
 #' counts of annotated matrices, taxa, phenotypes, characters, and states.
 #' 
-#' @param
-#' @return Summary counts of annotations in the Phenoscape KB
+#' @return A list of class "KBinfo" with summary statistics of annotation counts
+#'   and other KB metadata (specifically, a timestamp for the current KB build).
 #' @examples
-#'
+#' kbmeta <- get_KBinfo()
+#' names(kbmeta)
+#' kbmeta
+#' @export
 get_KBinfo <- function() {
-  request <- GET("https://kb.phenoscape.org/api/kb/annotation_summary")
-  response <- content(request, as = "text", encoding = "UTF-8")
-  summary <- fromJSON(response)
-  do.call("rbind", summary)
+  resp <- get_json_data(pkb_api("/kb/annotation_summary"), query = list())
+  for (timeElem in grep(".*_time", names(resp), value = TRUE)) {
+    resp[[timeElem]] <- strptime(resp[[timeElem]], format = "%FT%T", tz = "UTC")
+  }
+  class(resp) <- c(class(resp), "KBinfo")
+  resp
 }
 
-# I prefer to see the output formatted using the following line -- ok to add ? Or is there a different print format that's preferred?
-# cat("\nAnnotated taxa:", (summary$annotated_taxa[1]), "\nAnnotated characters:", (summary$annotated_characters[1]), "\nAnnotated states:", (summary$annotated_states[1]), "\nBuild date and time:", (summary$build_time[1]), "\n", "\n")
-
+#' Print method for objects of class KBinfo
+#'
+#' @param x the object of class "KBinfo" to be printed
+#' @param tz the timezone in which to print timestamp values, defaults to
+#'  local timezone
+#' @param ... additional paramaters that might be passed to print methods,
+#'  ignored here
+#' @rdname get_KBinfo
+#' @export
+print.KBinfo <- function(x, ..., tz = "") {
+  cat(sapply(names(x),
+             function(i) {
+               if (any(grepl("POSIX[c,l]t", class(x[[i]]))))
+                 val <- format(as.POSIXct(x[[i]]), usetz = TRUE, tz = tz)
+               else
+                 val <- x[[i]]
+               paste0(toupper(substring(i, 1, 1)),
+                      sub("_", " ", substring(i, 2)), ": ", val)
+             }),
+      sep = "\n")
+  invisible(x)
+}
