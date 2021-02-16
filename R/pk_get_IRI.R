b68099cba8b698c47abbe884d8289e95fcd72b29
John Bradley <johnbradley2008@gmail.com>
2021-02-16 14:20:31 -0500

    Replaces pk_GET calls with get_json_data
    pk_GET has been replaced by get_json_data

--- R/pk_get_IRI.R
+++ R/pk_get_IRI.R
@@ -70,8 +70,7 @@ find_term <- function(query,
   if (is.na(limit)) limit <- "1000000"
   queryseq <- c(queryseq, limit = limit)
 
-  res <- pk_GET(pkb_api("/term/search"), query = queryseq)
-
+  res <- get_json_data(pkb_api("/term/search"), query = queryseq)
   res <- res$results
 
   if (length(res) > 0 && nrow(res) > 0) {
